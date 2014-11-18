6be72fafd5f888e1c5e584ae2c163c3b406f4f77
Bartłomiej Filipek <bartlomiej.filipek@gmail.com>
2014-11-18 19:16:19 +0100

    code refactoring, x64 platform

--- cache2D/cache2d.cpp
+++ cache2D/cache2d.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include "TimeQuery.h"
 
-void test(const int ITERS, const int ARRAY_ROWS, const int ARRAY_COLS, bool RowOrder)
+void test(const int ITERS, const int ARRAY_ROWS, const int ARRAY_COLS, bool rowOrder)
 {
 	CpuTimeQuery cpuTime;
 
@@ -11,7 +11,7 @@ void test(const int ITERS, const int ARRAY_ROWS, const int ARRAY_COLS, bool RowO
 	int *array = new int[ARRAY_TOTAL_ELEMENTS];
 	memset(array, 0, ARRAY_TOTAL_SIZE);
 
-	if (RowOrder)
+	if (rowOrder)
 	{
 		cpuTime.begin();
 		for (int iter = 0; iter < ITERS; ++iter)
@@ -44,12 +44,9 @@ void test(const int ITERS, const int ARRAY_ROWS, const int ARRAY_COLS, bool RowO
 	array = nullptr;
 }
 
-int main(int argc, char *argv[])
+void runTest(const int ARRAY_MIN_SIZE, const int ARRAY_MAX_SIZE, const int ARRAY_STEP, const int ITERS, bool rowOrder)
 {
-	const int ARRAY_MIN_SIZE = argc > 1 ? atoi(argv[1]) : 100;
-	const int ARRAY_MAX_SIZE = argc > 1 ? atoi(argv[1]) : 1000;
-	const int ARRAY_STEP = argc > 2 ? atoi(argv[2]) : 100;
-	const int ITERS = 1000;
+	std::cout << (rowOrder ? "row order" : "column order") << std::endl;
 
 	std::cout << "\t";
 	for (int cols = ARRAY_MIN_SIZE; cols <= ARRAY_MAX_SIZE; cols += ARRAY_STEP)
@@ -61,8 +58,32 @@ int main(int argc, char *argv[])
 		std::cout << rows << "\t";
 		for (int cols = ARRAY_MIN_SIZE; cols <= ARRAY_MAX_SIZE; cols += ARRAY_STEP)
 		{
-			test(ITERS, rows, cols, false);
+			test(ITERS, rows, cols, rowOrder);
 		}
 		std::cout << std::endl;
-	}	
+	}
 }
+
+int main(int argc, char *argv[])
+{
+	if (argc == 1)
+	{
+		std::cout << "using default params!" << std::endl;
+		std::cout << "min_size, max_size, step" << std::endl << std::endl;
+	}
+
+	const int ARRAY_MIN_SIZE = argc > 1 ? atoi(argv[1]) : 100;
+	const int ARRAY_MAX_SIZE = argc > 1 ? atoi(argv[2]) : 1000;
+	const int ARRAY_STEP = argc > 2 ? atoi(argv[3]) : 100;
+	const int ITERS = 1000;
+
+	std::cout << "min size: " << ARRAY_MIN_SIZE << ", max size: " << ARRAY_MAX_SIZE << ", step: " << std::endl;
+	std::cout << "max array size: " << ARRAY_MAX_SIZE*ARRAY_MAX_SIZE*sizeof(int) << " bytes" << std::endl;
+
+	runTest(ARRAY_MIN_SIZE, ARRAY_MAX_SIZE, ARRAY_STEP, ITERS, true);
+	runTest(ARRAY_MIN_SIZE, ARRAY_MAX_SIZE, ARRAY_STEP, ITERS, false);
+
+	return 0;
+}
+
+
